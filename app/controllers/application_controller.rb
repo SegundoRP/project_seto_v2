7dfab2fcd04fec1b6b915023cbf2afe81eac2b25
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-05-04 13:57:08 -0500

    feat: :sparkles: add params to handle in devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :nickname, :first_name, :last_name, :cellphone])
+
+    # app/views/devise/sessions/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :nickname, :cellphone])
+  end
 end
