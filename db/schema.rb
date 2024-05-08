e3e1625f054081050449d8242a4058432dbc2ab1
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-05-07 23:32:00 -0500

    chore: :card_file_box: run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_02_011141) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_03_045959) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "consultations", force: :cascade do |t|
+    t.datetime "date", null: false
+    t.integer "status", default: 0, null: false
+    t.text "summary"
+    t.string "reason_consultation"
+    t.bigint "doctor_id", null: false
+    t.bigint "patient_id", null: false
+    t.bigint "type_of_service_id", null: false
+    t.decimal "price", precision: 7, scale: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["doctor_id"], name: "index_consultations_on_doctor_id"
+    t.index ["patient_id"], name: "index_consultations_on_patient_id"
+    t.index ["type_of_service_id"], name: "index_consultations_on_type_of_service_id"
+  end
 
   create_table "diagnostics", force: :cascade do |t|
     t.text "description", null: false
@@ -51,6 +67,29 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_02_011141) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "therapeutic_histories", force: :cascade do |t|
+    t.boolean "first_time_attention"
+    t.string "previous_diagnostic"
+    t.text "events_in_life"
+    t.text "previous_treatment"
+    t.text "familiar_antecedents"
+    t.bigint "patient_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["patient_id"], name: "index_therapeutic_histories_on_patient_id"
+  end
+
+  create_table "treatments", force: :cascade do |t|
+    t.interval "duration", null: false
+    t.date "start_time", null: false
+    t.text "about", null: false
+    t.bigint "diagnostic_id", null: false
+    t.integer "status", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["diagnostic_id"], name: "index_treatments_on_diagnostic_id"
+  end
+
   create_table "type_of_services", force: :cascade do |t|
     t.string "name_of_service", null: false
     t.datetime "created_at", null: false
@@ -72,6 +111,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_02_011141) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "consultations", "doctors"
+  add_foreign_key "consultations", "patients"
+  add_foreign_key "consultations", "type_of_services"
   add_foreign_key "diagnostics", "patients"
   add_foreign_key "doctors", "users"
+  add_foreign_key "therapeutic_histories", "patients"
+  add_foreign_key "treatments", "diagnostics"
 end
