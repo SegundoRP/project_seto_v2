4f05e7ad2d29cfb954cff00eb9371691ba95dabc
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-05-10 22:59:54 -0500

    chore: :globe_with_meridians: add es locale

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module ProjectSetoV2
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
+    config.i18n.default_locale = :es
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
