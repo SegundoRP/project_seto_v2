598973673e49bdb0b235ca58a12c49701247646c
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-05-10 22:59:29 -0500

    feat: :sparkles: add doctors controller

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,54 @@
+class DoctorsController < ApplicationController
+  before_action :doctor, only: %i[show edit update destroy]
+
+  def index
+    @doctors = Doctor.all
+
+    flash.now[:notice] = "Tienes #{@doctors.count}" + " especialista".pluralize(@doctors.count)
+  end
+
+  def show; end
+
+  def new
+    @doctor = Doctor.new
+  end
+
+  def create
+    @doctor = Doctor.new(doctor_params)
+
+    if @doctor.save
+      redirect_to doctor_path(@doctor), notice: t('.success')
+    else
+      render :new, status: :unprocessable_entity, alert: t(".failure")
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @doctor.update(doctor_params)
+      redirect_to doctor_path(@doctor), notice: t('.success')
+    else
+      render :edit, status: :unprocessable_entity, alert: t(".failure")
+    end
+  end
+
+  def destroy
+    if @doctor.destroy
+      redirect_to doctor_path(@doctor), notice: t('.success')
+    else
+      render :show, status: :unprocessable_entity, alert: t(".failure")
+    end
+  end
+
+  private
+
+  def doctor
+    @doctor ||= Doctor.find(params[:id])
+  end
+
+  def doctor_params
+    params.require(:doctor).permit(:first_name, :last_name, :age, :profession, :specialty, :college_number)
+          .merge(user: current_user)
+  end
+end
