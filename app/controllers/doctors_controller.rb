4c43413fa0a7e21058b3fc2dc04464427699f12c
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-06-03 00:00:38 -0500

    feat: :sparkles: modify doctors controller

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -3,7 +3,7 @@ class DoctorsController < ApplicationController
 
   def index
     @doctors = Doctor.all
-    # debugger
+
     flash.now[:notice] = "Tienes #{@doctors.count}" + " especialista".pluralize(@doctors.count)
   end
 
