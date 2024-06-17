7328fcb2262ea75394f2fed24f896709ba43f7fe
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-06-16 23:58:31 -0500

    test: :white_check_mark: add more tests for doctors controller

--- spec/requests/doctors_request_spec.rb
+++ spec/requests/doctors_request_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Doctors', :request do
-  describe 'GET #index' do
-    let(:user) { create(:user) }
-    let!(:doctor) { create(:doctor) }
+  let(:user) { create(:user) }
+  let!(:doctor) { create(:doctor) }
 
-    before do
-      sign_in(user)
-      get ''
-    end
+  before do
+    sign_in(user)
+  end
+
+  describe 'GET #index' do
+    before { get '' }
 
     it 'returns a success response' do
       expect(response).to have_http_status(:success)
@@ -16,6 +17,20 @@ RSpec.describe 'Doctors', :request do
 
     it 'returns total of doctors' do
       expect(Doctor.count).to eq(1)
+    end
+  end
+
+  describe 'GET #show' do
+    before { get doctor_path(doctor), params: { id: doctor.id } }
+
+    it 'returns a success response' do
+      p response
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns doctor information' do
+      expect(response.body).to include(doctor.first_name)
+      expect(assigns(doctor)).to eq(doctor)
     end
   end
 end
