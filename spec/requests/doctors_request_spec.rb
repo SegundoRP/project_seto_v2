2cff92543673b792bc3481ac7ad8b5b678ae3923
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-06-17 00:54:18 -0500

    test: :white_check_mark: add more request spec for doctors controller

--- spec/requests/doctors_request_spec.rb
+++ spec/requests/doctors_request_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'Doctors', :request do
   end
 
   describe 'GET #index' do
-    before { get '' }
+    before { get doctors_path }
 
     it 'returns a success response' do
       expect(response).to have_http_status(:success)
@@ -24,13 +24,31 @@ RSpec.describe 'Doctors', :request do
     before { get doctor_path(doctor), params: { id: doctor.id } }
 
     it 'returns a success response' do
-      p response
       expect(response).to have_http_status(:success)
     end
 
     it 'returns doctor information' do
       expect(response.body).to include(doctor.first_name)
-      expect(assigns(doctor)).to eq(doctor)
+    end
+  end
+
+  describe 'POST #create' do
+    context 'when record is valid' do
+      it 'returns a success response' do
+        post doctors_path, params: { doctor: { first_name: 'Second', last_name: 'Rebaza', profession: 'Psicologia',
+                                               user: } }
+        expect(response).to redirect_to(doctor_path(Doctor.last))
+        expect(flash[:notice]).to eq(I18n.t('doctors.create.success'))
+      end
+    end
+
+    context 'when record is invalid' do
+      it 'returns a failed response' do
+        post doctors_path, params: { doctor: { first_name: nil, last_name: nil, profession: nil } }
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(flash[:alert]).to eq(I18n.t('doctors.create.failure'))
+      end
     end
   end
 end
